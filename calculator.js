795a893d0c45d6ecb44fd90fdf8ac5cdc2a89bad
Hariom <hariom@rizilianttech.com>
2025-01-31 12:28:40 +0530

    add logic for custom delimiter

--- calculator.js
+++ calculator.js
@@ -2,19 +2,30 @@ const add = (input) => {
   //return 0 if input is empty string
   if (input === "") return 0;
 
-  //throw if non numeric number encontered
-    const notNumbers = input.split(/[\n,]/).filter((num) => isNaN(parseInt(num)));
-    if (notNumbers.length > 0) {
-      throw new Error(`non numeric value not allowed: ${notNumbers.join(",")}`);
-    }
+  if (input.startsWith("//")) {
+    const indexofDelimeterSectionEnd = input.indexOf("\n");
+    const delimeter = input.slice(2, indexofDelimeterSectionEnd);
+    const inputStr = input.slice(indexofDelimeterSectionEnd + 1);
 
-  //return the sum of numbers
-  return input
-    .split(/[\n,]/)
-    .map((num) => parseInt(num))
-    .reduce((total, num) => total + num);
+    return sum(inputStr, delimeter);
+  }
+
+  //throw if non numeric number encontered
+  const notNumbers = input.split(/[\n,]/).filter((num) => isNaN(parseInt(num)));
+  if (notNumbers.length > 0) {
+    throw new Error(`non numeric value not allowed: ${notNumbers.join(",")}`);
+  }
+
+  return sum(input, /[\n,]/);
 };
 
+function sum(input, delimeter) {
+  //return the sum of numbers
+  return input
+    .split(delimeter)
+    .map((num) => parseInt(num))
+    .reduce((total, num) => total + num);
+}
 
 module.exports = {
   add,
