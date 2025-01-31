9cca110b155cb72f5bc2de466798b1094964f86b
Hariom <hariom@rizilianttech.com>
2025-01-31 12:16:36 +0530

    add test case when custom delimeter given

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
 const add = (input) => {
   //return 0 if input is empty string
   if (input === "") return 0;
-  
+
   //throw if non numeric number encontered
-  const notNumbers = input.split(/[\n,]/).filter((num) => isNaN(parseInt(num)));
-  if (notNumbers.length > 0) {
-    throw new Error(`non numeric value not allowed: ${notNumbers.join(",")}`);
-  }
+    const notNumbers = input.split(/[\n,]/).filter((num) => isNaN(parseInt(num)));
+    if (notNumbers.length > 0) {
+      throw new Error(`non numeric value not allowed: ${notNumbers.join(",")}`);
+    }
 
   //return the sum of numbers
   return input
--- calculator.test.js
+++ calculator.test.js
@@ -25,4 +25,8 @@ describe("String Calculator", () => {
     expect(add("1\n2,3,4\n5")).toBe(15);
   });
 
+  it("handle custom delimiter", () => {
+    expect(add("//;\n1;2;3")).toBe(6);
+  });
+  
 });
