ea49d4edf0c404a99f6d4d21663973a7548d8f06
Hariom <hariom@rizilianttech.com>
2025-01-31 11:33:06 +0530

    add the logic for sum of numbers

--- calculator.js
+++ calculator.js
@@ -1,7 +1,13 @@
 const add = (input) => {
+  //return 0 if input is empty string
   if (input === "") return 0;
-};
 
+  //return the sum of numbers
+  return input
+    .split(",")
+    .map((num) => parseInt(num))
+    .reduce((total, num) => total + num);
+};
 
 module.exports = {
   add,
