e3cc0b8a38e91653327dae820d673846aac280c9
Hariom <hariom@rizilianttech.com>
2025-01-31 11:55:07 +0530

    handeled non numeric number

--- calculator.js
+++ calculator.js
@@ -2,14 +2,18 @@ const add = (input) => {
   //return 0 if input is empty string
   if (input === "") return 0;
 
+  //throw if non numeric number encontered
+  const notNumbers = input.split(",").filter((num) => isNaN(parseInt(num)));
+  if (notNumbers.length > 0) {
+    throw new Error(`non numeric value not allowed: ${notNumbers.join(",")}`);
+  }
+
   //return the sum of numbers
   return input
     .split(",")
     .map((num) => parseInt(num))
     .reduce((total, num) => total + num);
 };
-
-
 
 module.exports = {
   add,
